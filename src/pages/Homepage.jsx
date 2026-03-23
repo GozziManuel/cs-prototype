198f2e7cdbeaba885fc43cb731268fa0e35fb5b2
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-03-23 17:08:36 +0100

    finish Homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,3 +1,7 @@
+import { Link } from "react-router";
+import trips from "../data/trips";
+import TripCard from "../components/TripCard";
+
 export default function Homepage() {
   return (
     <>
@@ -8,6 +12,15 @@ export default function Homepage() {
           </div>
         </div>
         <div className="separator"></div>
+        <h2 className="mt-3">See in what trip you interested! </h2>
+        <div className="row g-3 mb-5">
+          {trips.slice(0, 3).map((el) => (
+            <TripCard el={el} key={el.id} />
+          ))}
+          <Link to={"/trips"} className="text-center card-link fs-3">
+            See more!
+          </Link>
+        </div>
       </div>
     </>
   );
