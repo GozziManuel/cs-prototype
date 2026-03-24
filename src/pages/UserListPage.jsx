5d690a8a088e11da9cf6e61f80777d129ff7c5df
micheleferri01 <micheleferri095@gmail.com>
2026-03-24 17:36:03 +0100

    add: total num of participants to the trip in UserListPage

--- src/pages/UserListPage.jsx
+++ src/pages/UserListPage.jsx
@@ -20,7 +20,7 @@ export default function UserListPage() {
     <>
       <div className="container">
         <h1 className="my-4 d-flex align-items-center gap-3">
-          Participants{" "}
+          Participants {`(${trip?.participants.length})`}
           <Link to="/trips" className="btn btn-primary">
             Go back to trip list
           </Link>
